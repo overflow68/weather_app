8d11d60fd6728fe3c6cd670dc35ac7ecd909a1c0
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-06-19 02:34:40 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    
+    
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      title: 'Weatherywhere',
+      filename: 'index.html',
+      inject:'body'
+    })
+  ],
+  
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        use:["style-loader","css-loader"]
+      },
+      {
+        test : /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpeg|jpg|png)$/,
+        use:[
+          {
+            loader: 'file-loader',
+            options:{
+              name:'[name].[ext]',
+              outputPath: 'images'
+            
+            }
+          }
+        ]
+      }
+      
+    ]
+  }
+};
