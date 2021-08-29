35ac3777878ffa8f6193cdf0afb9e59a70bcce70
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-08-29 16:57:18 +0100

    gh-pages dependencies

--- src/getData.js
+++ src/getData.js
@@ -4,7 +4,7 @@ const API_KEY = '96e6aaee7a6e3e2c63ff61c4089357bf';
 
 async function getCity(city) {
     try{
-     let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`, {mode: 'cors'})
+     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`, {mode: 'cors'})
      let cityData = await response.json();
      populateCityInfo(cityData);
      console.log(cityData);
